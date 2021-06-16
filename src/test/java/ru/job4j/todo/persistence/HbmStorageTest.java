8cf841bd933177b93b331fd633a032a7119861cf
roman <romankhiropulos@gmail.com>
2021-06-17 01:25:23 +0300

    ToDo codecov f.m.b.

---
+++ src/test/java/ru/job4j/todo/persistence/HbmStorageTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.todo.persistence;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HbmStorageTest {
+
+    @Test
+    public void saveItem() {
+        System.out.println("Hello world!");
+    }
+}
