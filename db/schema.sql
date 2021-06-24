d119e9a8156f973c36da285509787c3bc656a897
roman <romankhiropulos@gmail.com>
2021-06-24 20:06:37 +0300

    schema user - item

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,16 @@
 DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS j_user;
 
 CREATE TABLE item
 (
     id          SERIAL PRIMARY KEY,
     description VARCHAR   NOT NULL,
     created     TIMESTAMP NOT NULL,
-    done        BOOLEAN   NOT NULL DEFAULT FALSE
+    done        BOOLEAN   NOT NULL DEFAULT FALSE,
+    role_id int not null references j_user(id)
+);
+
+create table j_user (
+                        id serial primary key,
+                        name varchar(2000)
 );
